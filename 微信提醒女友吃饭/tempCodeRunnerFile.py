83c086b936c23bd3c48490023acd3cbd00b73bf4
Hellow world <36073454+EthanYe@users.noreply.github.com>
2019-04-22 16:25:32 +0800

    Add files via upload

---
+++ 微信提醒女友吃饭/tempCodeRunnerFile.py
@@ -0,0 +1,1 @@
+my_lady_wechat_name
---
+++ 微信提醒女友吃饭/test.py
@@ -0,0 +1,17 @@
+import os
+import xlwt
+import xlrd
+import sys
+
+# if('Windows' in system):
+#     print('hehe')
+# else:
+#     print('buhehe')
+
+# workbook = xlwt.Workbook(encoding='ascii')
+
+# worksheet = workbook.add_sheet('My Worksheet')
+# worksheet.write(0, 0, label='Row 0, Column 0 Value')
+# workbook.save('./vscode/wechat/Excel_Workbook.xls')
+
+print(os.path)
